9879ba75a71c59917c6c43d6072bf424c3202837
charlesroberts <charles@resultsathand.com>
2020-11-09 21:23:45 -0500

    Added code to give functionality to the search bar.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,19 +1,27 @@
 <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
 
+    
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
+                    <form action="search.php" method="post">
                     <div class="input-group">
-                        <input type="text" class="form-control">
+                        <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button class="btn btn-default" name="submit" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
                     </div>
+                    </form> <!-- Search form  -->
                     <!-- /.input-group -->
                 </div>
+
+
+
+
+
 
                 <!-- Blog Categories Well -->
                 <div class="well">
